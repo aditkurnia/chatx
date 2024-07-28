3890446b699b297562fc36bfc5413c1c11b77e67
AK Eleven <146339784+aditkurnia@users.noreply.github.com>
2024-07-28 15:57:24 +0700

    Add files via upload

--- MyApp/Chat.php
+++ MyApp/Chat.php
@@ -1,1 +1,29 @@
+<?php
+namespace MyApp;
 
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use PDO; // Untuk koneksi database
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+    protected $pdo;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+        $this->pdo = new PDO("mysql:host=localhost;dbname=your_database", "your_user", "your_password");
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $this->pdo->prepare("INSERT INTO messages (message) VALUES (?)")->execute([$msg]);
+        foreach ($this->clients as $client) {
+            $client->send($msg);
+        }
+    }
+
+    // ... (onClose, onError)
+}
