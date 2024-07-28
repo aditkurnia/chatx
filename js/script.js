1ec98915f97c7a7d029de8906031c8967d9a2d36
AK Eleven <146339784+aditkurnia@users.noreply.github.com>
2024-07-28 15:55:54 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+const socket = io('http://localhost:8080');
 
+$('form').submit(function(e) {
+    e.preventDefault();
+    const message = $('#m').val();
+    if (message.trim() !== '') {
+        socket.emit('chat message', message);
+        $('#m').val('');
+    }
+});
+
+socket.on('chat message', function(msg) {
+    $('#messages').append($('<li>').text(msg));
+    $('#chatbox').scrollTop($('#chatbox')[0].scrollHeight); // Auto-scroll
+});
