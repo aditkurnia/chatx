6723f1b274bbf2a1800d7b6c188b399a4aaca0c2
AK Eleven <146339784+aditkurnia@users.noreply.github.com>
2024-07-28 15:48:16 +0700

    add

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chat Real-Time</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="chatbox">
+        <ul id="messages"></ul>
+        <form action="">
+            <input id="m" autocomplete="off" /><button>Kirim</button>
+        </form>
+    </div>
+    <script src="https://cdn.socket.io/4.5.4/socket.io.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
